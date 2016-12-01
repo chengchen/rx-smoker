50232c7003b62cbc6b5dcc90f0ec5669e29f6727
Chengchen Ji <cji@edgelab.ch>
2016-12-01 22:55:56 +0100

    remove the flyway

--- src/main/java/com/edgelab/marketdata/consumer/StockConsumer.java
+++ src/main/java/com/edgelab/marketdata/consumer/StockConsumer.java
@@ -1,25 +1,13 @@
 package com.edgelab.marketdata.consumer;
 
 import lombok.RequiredArgsConstructor;
-import org.reactivestreams.Publisher;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 @Service
 @RequiredArgsConstructor
 public class StockConsumer {
 
     private final StockQuotationRepository repository;
-
-    public void save(Publisher<StockQuotation> stockQuotations) {
-        Flux.from(stockQuotations)
-            .buffer(100)
-            .flatMap(quotes -> Mono.fromCallable(() -> repository.save(quotes))
-                .subscribeOn(Schedulers.parallel()))
-            .subscribe();
-    }
 
     public StockQuotation save(StockQuotation stockQuotation) {
         return repository.save(stockQuotation);
