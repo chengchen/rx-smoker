38a1750ef31c8c4276b43c688366e12a9b98c138
Chengchen Ji <cji@edgelab.ch>
2016-12-08 17:29:31 +0100

    use single subscribe

--- src/main/java/com/edgelab/marketdata/ApiController.java
+++ src/main/java/com/edgelab/marketdata/ApiController.java
@@ -7,14 +7,12 @@ import com.edgelab.marketdata.publisher.StockPublisher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -36,15 +34,14 @@ public class ApiController {
         List<String> tickers = Arrays.asList("T", "AAPL", "OHI", "MAP.MC", "SAN.MC", "ABBN", "UBSG", "BABA");
 
         Flux.from(stockPublisher.fetchQuotes(tickers))
-            .flatMap(stockQuotation -> Mono.fromRunnable(() ->
-                stockConsumer.save(stockQuotation, r -> {
-                    try {
-                        emitter.send(r, MediaType.APPLICATION_JSON);
-                    } catch (IOException e) {
-                        log.error("fuck me");
-                    }
-                })
-            ).subscribeOn(Schedulers.parallel()))
+            .flatMap(stockQuotation -> Mono.fromRunnable(() -> {
+                try {
+                    stockConsumer.save(stockQuotation);
+                    emitter.send(stockQuotation);
+                } catch (Exception e) {
+                    log.error("fuck me");
+                }
+            }))
             .doOnTerminate(emitter::complete)
             .subscribe();
 
--- src/main/java/com/edgelab/marketdata/consumer/StockConsumer.java
+++ src/main/java/com/edgelab/marketdata/consumer/StockConsumer.java
@@ -3,18 +3,13 @@ package com.edgelab.marketdata.consumer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.FluxSink.OverflowStrategy;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 import javax.annotation.PostConstruct;
 import java.time.Duration;
-import java.util.function.Consumer;
-
-import static java.util.function.Function.identity;
 
 @Service
 @RequiredArgsConstructor
@@ -24,23 +19,17 @@ public class StockConsumer {
     private final StockQuotationRepository repository;
 
     private FluxSink<StockQuotation> quotesSink;
-    private ConnectableFlux<StockQuotation> savedQuotes;
 
     @PostConstruct
     private void setUpServiceCallFlux() {
-        savedQuotes = Flux.<StockQuotation>create(emitter -> quotesSink = emitter.serialize(), OverflowStrategy.ERROR)
+        Flux.<StockQuotation>create(emitter -> quotesSink = emitter.serialize(), OverflowStrategy.ERROR)
+            .log()
             .buffer(100, Duration.ofSeconds(10))
-            .flatMap(quotes -> Mono.fromSupplier(() -> repository.save(quotes))
-                .subscribeOn(Schedulers.parallel())
-            )
-            .flatMapIterable(identity())
-            .publish();
+            .flatMap(quotes -> Mono.fromSupplier(() -> repository.save(quotes)))
+            .subscribe();
     }
 
-    public void save(StockQuotation stockQuotation, Consumer<StockQuotation> consumer) {
-        savedQuotes.filter(quote -> quote.toString().equals(stockQuotation.toString()))
-            .subscribe(consumer);
-        savedQuotes.connect();
+    public void save(StockQuotation stockQuotation) {
         quotesSink.next(stockQuotation);
     }
 
