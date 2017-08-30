4a8172d2e5d3ff2076a84a5b4047fd99c5e3b875
Chengchen JI <jichengchen1988@gmail.com>
2017-08-30 22:05:03 +0200

    reduce the number of time series

--- src/main/java/com/edgelab/marketdata/ApiController.java
+++ src/main/java/com/edgelab/marketdata/ApiController.java
@@ -5,6 +5,7 @@ import com.edgelab.marketdata.domain.QuoteService;
 import com.edgelab.marketdata.domain.Stock;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
@@ -19,7 +20,7 @@ public class ApiController {
 
     @GetMapping(value = "/feeds")
     public void feedQuotes() {
-        Flux.range(1, 10000)
+        Flux.range(1, 1000)
             .map(this::buildStock)
             .flatMap(this::buildQuotes)
             .doOnNext(quote -> quoteService.write(quote.getTicker(), quote.getDate(), quote.getValue()))
@@ -31,13 +32,13 @@ public class ApiController {
     }
 
     private Flux<Quote> buildQuotes(Stock stock) {
-        return Flux.range(1, 10000)
+        return Flux.range(1, 4000)
             .map(i -> new Quote(stock.getTicker(), Instant.now().plusSeconds(i), Math.random()));
     }
 
-    @GetMapping(value = "/fetch")
-    public List<Quote> fetchQuotes() {
-        return quoteService.fetchQuotes("1555");
+    @GetMapping(value = "/fetch/{ticker}")
+    public List<Quote> fetchQuotes(@PathVariable String ticker) {
+        return quoteService.fetchQuotes(ticker);
     }
 
 }
