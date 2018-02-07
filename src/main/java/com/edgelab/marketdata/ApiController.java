d751ac04584abb571ffc2f39d44e8a495844082d
Chengchen Ji <cji@edgelab.ch>
2018-02-07 21:16:24 +0100

    clean up

--- src/main/java/com/edgelab/marketdata/ApiController.java
+++ src/main/java/com/edgelab/marketdata/ApiController.java
@@ -1,52 +1,30 @@
 package com.edgelab.marketdata;
 
-import com.edgelab.marketdata.domain.Quote;
-import com.edgelab.marketdata.domain.QuoteService;
 import com.edgelab.marketdata.domain.Stock;
+import com.edgelab.marketdata.domain.StockRepository;
 import com.google.common.base.Stopwatch;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 
-import java.time.Instant;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ApiController {
 
-    private final QuoteService quoteService;
-
-    private final Instant now = Instant.now();
+    private final StockRepository stockRepository;
 
     @GetMapping(value = "/feeds")
-    public String feedQuotes() {
+    public Iterable<Stock> feedQuotes() {
         Stopwatch stopwatch = Stopwatch.createStarted();
 
-        Flux.range(1, 500_000)
-            .map(this::buildStock)
-            .flatMap(this::buildQuotes)
-            .doOnNext(quote -> quoteService.write(quote.getTicker(), quote.getDate(), quote.getValue()))
-            .subscribe();
+        Iterable<Stock> stocks = stockRepository.findAll();
+        log.info("Elapsed: " + stopwatch.elapsed(TimeUnit.SECONDS));
 
-        return "Elapsed: " + stopwatch.elapsed(TimeUnit.SECONDS);
-    }
-
-    private Stock buildStock(Integer i) {
-        return new Stock(i, i.toString());
-    }
-
-    private Flux<Quote> buildQuotes(Stock stock) {
-        return Flux.range(1, 4000)
-            .map(i -> new Quote(stock.getTicker(), now.plusSeconds(i), i.doubleValue()));
-    }
-
-    @GetMapping(value = "/fetch/{ticker}")
-    public List<Quote> fetchQuotes(@PathVariable String ticker) {
-        return quoteService.fetchQuotes(ticker);
+        return stocks;
     }
 
 }
--- src/main/java/com/edgelab/marketdata/domain/StockRepository.java
+++ src/main/java/com/edgelab/marketdata/domain/StockRepository.java
@@ -1,10 +1,8 @@
 package com.edgelab.marketdata.domain;
 
-import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@JaversSpringDataAuditable
 public interface StockRepository extends PagingAndSortingRepository<Stock, Integer> {
 }
